e2bc7ecde7ab91c98d2833d41f94c11303e5d707
asitanc <adam.sitanc@gmail.com>
2022-10-28 14:02:21 +0200

    add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,15 +1,28 @@
 /** @type {import('next').NextConfig} */
 const assetPrefix =  ''
+const isProd = process.env.NODE_ENV === 'production'
+const withPlugins = require("next-compose-plugins")
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 
 const settings = {
+  i18n: {
+    locales: ['en_US'],
+    defaultLocale: 'en_US'
+  },
   trailingSlash: true,
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone',
   typescript: {
     ignoreBuildErrors: true,
   },
+  assetPrefix: isProd ? '' : undefined,
   webpack: (config, { dev }) => {
     config.output.publicPath = `${assetPrefix}${config.output.publicPath}`
     return config
   }
 }
 
-module.exports = settings
+module.exports = withPlugins([withBundleAnalyzer], settings)
